f387735a7785e45d395fce6ddb1633569388f138
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2026-04-07 08:37:25 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
 create table products (
-  id uuid primary key default uuid_generate_v4(),
-  nama text,
-  harga_modal_rp numeric
+  id serial primary key,
+  nama text
+);
+
+create table stock_masuk (
+  id serial primary key,
+  product_id int,
+  jumlah int,
+  tanggal date
+);
+
+create table invoice_detail (
+  id serial primary key,
+  product_id int,
+  jumlah int,
+  harga_jual_rp numeric
 );
